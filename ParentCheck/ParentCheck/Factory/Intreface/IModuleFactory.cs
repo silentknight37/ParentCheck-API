8d5843247ee99683d780f974ffa3a28e9c49a869
Shanaka Kariyawasam <shanaka.kariyawasam@gmail.com>
2021-05-19 22:37:42 +0530

    add factory stucture

---
+++ ParentCheck/ParentCheck/Factory/Intreface/IModuleFactory.cs
@@ -0,0 +1,10 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace ParentCheck.Repository.Intreface
+{
+    interface IModuleFactory
+    {
+    }
+}
---
+++ ParentCheck/ParentCheck/Factory/RoleModuleFactory.cs
@@ -0,0 +1,11 @@
+﻿using ParentCheck.Repository.Intreface;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace ParentCheck.Repository
+{
+    class RoleModuleFactory : IRoleModuleFactory
+    {
+    }
+}
